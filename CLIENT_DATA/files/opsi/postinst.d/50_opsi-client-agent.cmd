9f9a2d2673763e054b3cca9c2ff6ae4457d8c276
Detlef Oertel <d.oertel@uib.de>
2018-04-13 14:00:11 +0200

    opsi-client-agent (4.1.0.0-1) stable; urgency=low
      * added opsiclientdguard service
      * upgrade to opsi-winst 4.12.0.16
      * setup script is setup411.opsiscript (change also in postinst.d 50_oca)
      * fork setup scripts by winst version >= 4.12.0.16 calls setup412.opsiscript
      * notifier upgrade to 4.1.0.0
      * notifier configuration now in opsi-notifier/notifier.d
      * systray upgrade 4.1.0.0
      * update to openssl-1.0.2o
      * kiosk upgrade 4.1.0.0
    
    -- Detlef Oertel <d.oertel@uib.de>  Fri,  16 Mar 2018:15:00:00 +0200

--- CLIENT_DATA/files/opsi/postinst.d/50_opsi-client-agent.cmd
+++ CLIENT_DATA/files/opsi/postinst.d/50_opsi-client-agent.cmd
@@ -3,6 +3,6 @@ rem echo [%date% %time%] copy 64 Bit cmd.exe for use under 32 Bit
 rem copy "%SYSTEMROOT%\system32\cmd.exe" "%SYSTEMROOT%\cmd64.exe"
 rem --- Install opsi-client-agent
 echo [%date% %time%] Starting opsi-client-agent installation as user %USERNAME%
-echo [%date% %time%] Executing: "%script_drive%\opsi\opsi-client-agent\files\opsi\opsi-winst\winst32.exe" /batch "%script_drive%\opsi\opsi-client-agent\files\opsi\setup412.opsiscript" %sys_drive%\opsi.org\log\opsi-client-agent.log /PARAMETER INSTALL:BOOTIMAGE
-"%script_drive%\opsi\opsi-client-agent\files\opsi\opsi-winst\winst32.exe" /batch "%script_drive%\opsi\opsi-client-agent\files\opsi\setup412.opsiscript" %sys_drive%\opsi.org\log\opsi-client-agent.log /PARAMETER INSTALL:BOOTIMAGE
+echo [%date% %time%] Executing: "%script_drive%\opsi\opsi-client-agent\files\opsi\opsi-winst\winst32.exe" /batch "%script_drive%\opsi\opsi-client-agent\files\opsi\setup411.opsiscript" %sys_drive%\opsi.org\log\opsi-client-agent.log /PARAMETER INSTALL:BOOTIMAGE
+"%script_drive%\opsi\opsi-client-agent\files\opsi\opsi-winst\winst32.exe" /batch "%script_drive%\opsi\opsi-client-agent\files\opsi\setup411.opsiscript" %sys_drive%\opsi.org\log\opsi-client-agent.log /PARAMETER INSTALL:BOOTIMAGE
 
