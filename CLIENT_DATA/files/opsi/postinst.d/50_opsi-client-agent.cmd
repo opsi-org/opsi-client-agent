1ce847d338a93fdfa795ae6ab461e5cb18ab8542
Jan Schneider <j.schneider@uib.de>
2021-04-21 09:23:22 +0200

    [chg] Delete old files

--- CLIENT_DATA/files/opsi/postinst.d/50_opsi-client-agent.cmd
+++
@@ -1,8 +0,0 @@
-
-rem echo [%date% %time%] copy 64 Bit cmd.exe for use under 32 Bit
-rem copy "%SYSTEMROOT%\system32\cmd.exe" "%SYSTEMROOT%\cmd64.exe"
-rem --- Install opsi-client-agent
-echo [%date% %time%] Starting opsi-client-agent installation as user %USERNAME%
-echo [%date% %time%] Executing: "%script_drive%\opsi\opsi-client-agent\files\opsi\opsi-winst\winst32.exe" /batch "%script_drive%\opsi\opsi-client-agent\files\opsi\setup.opsiscript" %sys_drive%\opsi.org\log\opsi-client-agent.log /PARAMETER INSTALL:BOOTIMAGE
-"%script_drive%\opsi\opsi-client-agent\files\opsi\opsi-winst\winst32.exe" /batch "%script_drive%\opsi\opsi-client-agent\files\opsi\setup.opsiscript" %sys_drive%\opsi.org\log\opsi-client-agent.log /PARAMETER INSTALL:BOOTIMAGE
-
--- CLIENT_DATA/files/opsi/uninst/deactivate.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-echo "deactivate opsi-client-agent on %COMPUTERNAME% ?"
-pause
-
-..\opsi-winst\winst32.exe /batch uninstall.opsiscript "c:\opsi.org\log\deactivate_opsi-client-agent.log" /PARAMETER SRV_OFF
-
-echo "please reboot ...." 
-pause
