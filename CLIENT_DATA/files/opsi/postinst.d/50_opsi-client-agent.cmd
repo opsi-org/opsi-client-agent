238f1d3008555c4aadfd52481a5327364ba45465
Jan Schneider <j.schneider@uib.de>
2021-04-25 22:49:51 +0200

    [fix] Fixes

--- CLIENT_DATA/files/opsi/postinst.d/50_opsi-client-agent.cmd
+++ CLIENT_DATA/files/opsi/postinst.d/50_opsi-client-agent.cmd
@@ -2,10 +2,14 @@ rem --- Install opsi-client-agent
 echo [%date% %time%] Starting opsi-client-agent installation as user %USERNAME%
 IF EXIST %script_drive%\opsi\opsi-client-agent\files\opsi\cfg\config.ini (
 	FOR /F "tokens=1* delims==" %%A IN (%script_drive%\opsi\opsi-client-agent\files\opsi\cfg\config.ini) DO (
-		IF "%%A"=="service_address" set service_address=%%B
-		IF "%%A"=="client_id" set client_id=%%B
-		IF "%%A"=="client_key" set client_key=%%B
+		IF "%%A"=="bootserver" set bootserver=%%B
+		IF "%%A"=="hostname" set hostname=%%B
+		IF "%%A"=="dnsdomain" set dnsdomain=%%B
+		IF "%%A"=="pckey" set pckey=%%B
 	)
+	set service_address=%bootserver%
+	set client_id=%hostname%.%dnsdomain%
+	set client_key=%pckey%
 )
 IF EXIST %script_drive%\opsi\opsi-client-agent\install.conf (
 	FOR /F "tokens=1* delims==" %%A IN (%script_drive%\opsi\opsi-client-agent\install.conf) DO (
