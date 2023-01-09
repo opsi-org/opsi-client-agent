70548e084bc2ddecf9c8440654defe1674ce99d4
doerrer <n.doerrer@uib.de>
2023-01-09 17:57:51 +0100

    [chg] differentiate between new and old bootimage/patcha behaviour

--- CLIENT_DATA/files/opsi/postinst.d/50_opsi-client-agent.cmd
+++ CLIENT_DATA/files/opsi/postinst.d/50_opsi-client-agent.cmd
@@ -1,30 +1,39 @@
 rem --- Install opsi-client-agent
-setlocal EnableDelayedExpansion
-if "%client_agent_dir%"=="" set client_agent_dir=%script_drive%\opsi\opsi-client-agent
-
 echo [%date% %time%] Starting opsi-client-agent installation as user %USERNAME%
-if exist "%client_agent_dir%\install.conf" (
-	echo [%date% %time%] install.conf found
-	for /F "tokens=1* delims==" %%a IN (%client_agent_dir%\install.conf) do (
-		set option=%%a
-		set value=%%b
-		if "!option: =!"=="service_address" set service_address=!value: =!
-		if "!option: =!"=="client_id" set client_id=!value: =!
-		if "!option: =!"=="client_key" set client_key=!value: =!
-	)
-)
-if exist "%client_agent_dir%\files\opsi\cfg\config.ini" (
-	echo [%date% %time%] config.ini found
-	for /F "tokens=1* delims==" %%a IN (%client_agent_dir%\files\opsi\cfg\config.ini) do (
-		set option=%%a
-		set value=%%b
-		if "!option: =!"=="service_address" set service_address=!value: =!
-		if "!option: =!"=="client_hostname" set client_hostname=!value: =!
-		if "!option: =!"=="client_domain" set client_domain=!value: =!
-		if "!option: =!"=="client_key" set client_key=!value: =!
-	)
-	set client_id=!client_hostname!.!client_domain!
-)
 
-echo [%date% %time%] Executing: "%client_agent_dir%\files\opsi-script\opsi-script.exe" "%client_agent_dir%\setup.opsiscript" "%sys_drive%\opsi.org\log\opsi-client-agent.log" /batch /productid "opsi-client-agent" /opsiservice "%service_address%" /clientid "%client_id%" /username "%client_id%" /password "%client_key%" /parameter "bootimage"
-"%client_agent_dir%\files\opsi-script\opsi-script.exe" "%client_agent_dir%\setup.opsiscript" "%sys_drive%\opsi.org\log\opsi-client-agent.log" /batch /productid "opsi-client-agent" /opsiservice "%service_address%" /clientid "%client_id%" /username "%client_id%" /password "%client_key%" /parameter "bootimage"
+set client_id={{client_id}}
+rem if client_id is not automatically replaced by patcha, use old method
+if %client_id:~0,2%=="{{" (
+	setlocal EnableDelayedExpansion
+	if exist "%script_drive%\opsi\opsi-client-agent\install.conf" (
+		echo [%date% %time%] install.conf found
+		for /F "tokens=1* delims==" %%a IN (%script_drive%\opsi\opsi-client-agent\install.conf) do (
+			set option=%%a
+			set value=%%b
+			if "!option: =!"=="service_address" set service_address=!value: =!
+			if "!option: =!"=="client_id" set client_id=!value: =!
+			if "!option: =!"=="client_key" set client_key=!value: =!
+		)
+	)
+	if exist "%script_drive%\opsi\opsi-client-agent\files\opsi\cfg\config.ini" (
+		echo [%date% %time%] config.ini found
+		for /F "tokens=1* delims==" %%a IN (%script_drive%\opsi\opsi-client-agent\files\opsi\cfg\config.ini) do (
+			set option=%%a
+			set value=%%b
+			if "!option: =!"=="service_address" set service_address=!value: =!
+			if "!option: =!"=="client_hostname" set client_hostname=!value: =!
+			if "!option: =!"=="client_domain" set client_domain=!value: =!
+			if "!option: =!"=="client_key" set client_key=!value: =!
+		)
+		set client_id=!client_hostname!.!client_domain!
+	)
+
+	echo [%date% %time%] Executing: "%script_drive%\opsi\opsi-client-agent\files\opsi-script\opsi-script.exe" "%script_drive%\opsi\opsi-client-agent\setup.opsiscript" "%sys_drive%\opsi.org\log\opsi-client-agent.log" /batch /productid "opsi-client-agent" /opsiservice "%service_address%" /clientid "%client_id%" /username "%client_id%" /password "%client_key%" /parameter "bootimage"
+	"%script_drive%\opsi\opsi-client-agent\files\opsi-script\opsi-script.exe" "%script_drive%\opsi\opsi-client-agent\setup.opsiscript" "%sys_drive%\opsi.org\log\opsi-client-agent.log" /batch /productid "opsi-client-agent" /opsiservice "%service_address%" /clientid "%client_id%" /username "%client_id%" /password "%client_key%" /parameter "bootimage"
+)
+else (
+	echo [%date% %time%] Executing: "{{client_agent_dir}}\files\opsi-script\opsi-script.exe" "{{client_agent_dir}}\setup.opsiscript" "%sys_drive%\opsi.org\log\opsi-client-agent.log" /batch /productid "opsi-client-agent" /opsiservice "{{service_address}}" /clientid "{{client_id}}" /username "{{client_id}}" /password "{{client_key}}" /parameter "bootimage"
+	"{{client_agent_dir}}\files\opsi-script\opsi-script.exe" "{{client_agent_dir}}\setup.opsiscript" "%sys_drive%\opsi.org\log\opsi-client-agent.log" /batch /productid "opsi-client-agent" /opsiservice "{{service_address}}" /clientid "{{client_id}}" /username "{{client_id}}" /password "{{client_key}}" /parameter "bootimage"
+	rem echo [%date% %time%] Executing: "{{client_agent_dir}}\oca-installation-helper.exe" --service-address "{{service_address}}" --service-username "{{client_id}}" --service-password "{{client_key}}" --client-id "{{client_id}}" --non-interactive
+	rem "{{client_agent_dir}}\oca-installation-helper.exe" --service-address "{{service_address}}" --service-username "{{client_id}}" --service-password "{{client_key}}" --client-id "{{client_id}}" --non-interactive
+)
