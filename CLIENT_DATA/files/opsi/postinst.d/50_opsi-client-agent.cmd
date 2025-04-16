6481aa5210e87c1339119093f48aa39f164d51ee
doerrer <n.doerrer@uib.de>
2025-04-16 08:11:21 +0200

    [fix] fixed oca postinst for netboot

--- CLIENT_DATA/files/opsi/postinst.d/50_opsi-client-agent.cmd
+++ CLIENT_DATA/files/opsi/postinst.d/50_opsi-client-agent.cmd
@@ -28,9 +28,9 @@ if %client_id:~0,2%=={{ (
 		set client_id=!client_hostname!.!client_domain!
 	)
 
-	echo [%date% %time%] Executing: "%script_drive%\opsi\opsi-client-agent\oca-installation-helper.exe" "--service-address "!service_address!" --client-id "!client_id!" --service-username "!client_id!" --service-password "!client_key!" --bootimage"
-	"%script_drive%\opsi\opsi-client-agent\\oca-installation-helper.exe" "--service-address" !service_address! "--client-id" !client_id! "--service-username" !client_id! "--service-password" !client_key! "--bootimage"
+	echo [%date% %time%] Executing: "%script_drive%\opsi\opsi-client-agent\oca-installation-helper.exe" "--non-interactive" "--service-address "!service_address!" --client-id "!client_id!" --service-username "!client_id!" --service-password "!client_key!" --bootimage"
+	"%script_drive%\opsi\opsi-client-agent\\oca-installation-helper.exe" "--non-interactive" "--service-address" !service_address! "--client-id" !client_id! "--service-username" !client_id! "--service-password" !client_key! "--bootimage"
 ) else (
-	echo [%date% %time%] Executing: "{{client_agent_dir}}\oca-installation-helper.exe" "--service-address "!service_address!" --client-id "!client_id!" --service-username "!client_id!" --service-password "!client_key!" --bootimage"
-	"{{client_agent_dir}}\oca-installation-helper.exe" "--service-address" !service_address! "--client-id" !client_id! "--service-username" !client_id! "--service-password" !client_key! "--bootimage"
+	echo [%date% %time%] Executing: "{{client_agent_dir}}\oca-installation-helper.exe" "--non-interactive" "--service-address "!service_address!" --client-id "!client_id!" --service-username "!client_id!" --service-password "!client_key!" --bootimage"
+	"{{client_agent_dir}}\oca-installation-helper.exe" "--non-interactive" "--service-address" !service_address! "--client-id" !client_id! "--service-username" !client_id! "--service-password" !client_key! "--bootimage"
 )
